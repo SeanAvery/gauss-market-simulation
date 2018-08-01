7acef78cafdea474fb723cef12b292443062f3b0
Sean Pollock <seanpollock3344@gmail.com>
2018-08-01 04:38:58 -0400

    webpack dev server setup

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   entry: `${__dirname}/src/${index.js}`,
   output: `${__dirname}/dist.js`,
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
     rules: [
       {
@@ -12,7 +15,7 @@ module.exports = {
           options: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ['@babel/preset-env', '@babel/preset-react'],
               plugins: [require('@babel/plugin-proposal-object-rest-spread')]
             }
           }
